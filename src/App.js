8118e56b6c7e1387b1f83a7bb38f556b68fb48c4
Rodrigo Silvério <silverio_tenor@outlook.com>
2020-12-23 19:11:02 -0300

    Configuraçao dos metodos para listar, adicionar e remover repositorios na aplicaçao, bem como a estruturaçao do front-end.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then(response => setRepositories(response.data));
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('/repositories', {
+      "title": `Novo repositório - ${Date.now()}`,
+      "url": `https://github.com/silverioTenor/${Date.now()}`,
+      "techs": ["Node.js", "React", "React Native"]
+    });
+
+    const respository = response.data;
+
+    setRepositories([...repositories, respository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const foundIndex = repositories.findIndex(repository => repository.id === id);
+    
+    if (foundIndex < 0) return
+
+    await api.delete(`/repositories/${id}`);
+
+    repositories.splice(foundIndex, 1);
+
+    setRepositories([...repositories]);
   }
 
   return (
-    <div>
+    <div className="container">
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            <div className="content">
+              <h4>{repository.title}</h4>
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+              <p>{repository.url}</p>
+
+              <div id="techs">
+                {repository.techs.map(tech => (
+                  <span key={tech}>{tech}</span>
+                ))}
+              </div>
+            </div>
+
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
+      <button className="add" onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
 }
